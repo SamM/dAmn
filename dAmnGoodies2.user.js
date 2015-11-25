200b9587235b1eb0e3c91e7950767d1350c4bf61
Sam Mulqueen <sam.mulqueen.nz@gmail.com>
2015-11-26 03:06:34 +1300

    Userscript not updating

--- dAmnGoodies2.user.js
+++ dAmnGoodies2.user.js
@@ -4,7 +4,7 @@
 // @name           dAmnGoodies
 // @description    Novelty features for dAmn chat.
 // @author         Sam Mulqueen <sammulqueen.nz@gmail.com>
-// @version        2.2.4
+// @version        2.2.5
 // @include        http://chat.deviantart.com/chat/*
 // ==/UserScript==
 
@@ -15,7 +15,7 @@ function init(){
 		throw "Aw hell no";
 	}
 	var dAmnGoodies = new (function(){
-		this.version = "2.2.4";
+		this.version = "2.2.5";
 
 		var DG = this;
 
