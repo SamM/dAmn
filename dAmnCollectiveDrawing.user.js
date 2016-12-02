5198c64371becf33a1da2360f15aefcc9f43dfa7
Sam Mulqueen <sam.mulqueen.nz@gmail.com>
2016-12-02 19:04:22 +1300

    Seperated dAmn methods into a new file and updated the events system

--- dAmnCollectiveDrawing.user.js
+++ dAmnCollectiveDrawing.user.js
@@ -2,7 +2,7 @@
 // @name           dAmn Collective Drawing
 // @description    Draw alongside other Deviants right from within dAmn
 // @author         Sam Mulqueen <sammulqueen.nz@gmail.com>
-// @version        0.0.2
+// @version        0.0.3
 // @include        http://chat.deviantart.com/chat/*
 // ==/UserScript==
 
@@ -13,75 +13,6 @@ function DCDScript(){
 
   dAmn.original = {};
   dAmn.replaced = {};
-
-  var dAmn_methods = [
-    "dAmn_superdAmn_detect", "dAmn_ChatPage_init", "dAmn_Init",
-    "dAmn_AddLogCB", "dAmn_RemLogCB", "dAmn_Connect",
-    "dAmn_Login", "dAmn_Logoff", "dAmn_Rem_dAmnCB",
-    "dAmn_Add_dAmnCB", "dAmn_Join", "dAmn_Part", "dAmn_Send",
-    "dAmn_GetAttention", "dAmn_Raw", "dAmn_Set", "dAmn_Get",
-    "dAmn_Kick", "dAmn_Kill", "dAmn_ParseArgsNData",
-    "dAmn_ParsePacket", "dAmn_ExceptionPrint",
-    "dAmn_arrayRemoveIndex", "dAmn_arrayFindValue",
-    "dAmn_arrayAddUnique", "dAmn_arrayRemValue",
-    "dAmn_arrayForEach", "dAmn_objForEach",
-    "dAmn_objForEachString", "dAmn_objCount",
-    "dAmn_stringIEqual", "dAmn_print2Digits",
-    "dAmn_printDate", "dAmn_printElapsedTime",
-    "dAmn_printElement", "dAmn_doInsertAdjacentHTML",
-    "dAmn_StringizePacket", "dAmn_DeleteChildren",
-    "dAmn_DeleteSelf", "dAmn_classMatch",
-    "dAmn_findParentWClass", "dAmn_MakeEl", "dAmn_MakeDiv",
-    "dAmn_MakeSpan", "dAmn_MakeTextInput", "dAmn_MakeButton",
-    "dAmn_AddEl", "dAmn_InsertEl", "dAmn_AddDiv",
-    "dAmn_AddSpan", "dAmn_AddNewEl", "dAmn_getOverflow",
-    "dAmn_getDisplay", "dAmn_getPosition",
-    "dAmn_getBorderBottomWidth", "dAmn_cursorToEnd",
-    "dAmn_resizeFlexyRow", "dAmn_resizeFlexyCol",
-    "dAmn_resizeInverted", "dAmn_noHTML", "dAmn_Plugin_Java",
-    "dAmn_Plugin_XPCOM", "dAmn_Plugin_Flash",
-    "dAmn_PluginObj", "dAmn_Plugin_jsPing_timeout",
-    "dAmn_Plugin_jsWait_timeout", "dAmn_GetElement",
-    "dAmn_AutoPos_ResizeEvent", "dAmn_Log", "dAmn_PopQueue",
-    "dAmn_ProcessQueues", "dAmn_Command", "dAmn_Cmd",
-    "dAmn_Command_TrySend", "dAmn_GenerateParsedPacket",
-    "dAmn_Client_ReConnect", "dAmnFlash_DoFSCommand",
-    "dAmnJava_DoCommand_real", "dAmnJava_DoCommand",
-    "dAmn_DoCommand", "dAmnChat_Init", "dAmn_formatNS",
-    "dAmn_format_pchat_ns", "dAmnChatbase_dAmnCB",
-    "dAmnChat_Remove", "dAmnChatTabs_rem",
-    "dAmnChatTabs_activate_active", "dAmnChatTabs_activate",
-    "dAmnChatTabs_activateNext", "dAmnChatTabs_flashTab",
-    "dAmnChatTabs_newData", "dAmn_InvalidateLayout",
-    "dAmn_CalculateLayout", "dAmnChatbase_onResize",
-    "dAmnChatbase_onResize_cb", "dAmnChatbase_onLog",
-    "dAmnChatbase_imgbox_onclick",
-    "dAmnChat_ImgBoxHover_enter", "dAmnChat_ImgBoxHover_exit",
-    "dAmnChat_AddImgBox_Images", "dAmnChat_AddImgBox",
-    "dAmnChatbase_onObjClose", "dAmnEscape", "dAmnChat",
-    "dAmnChat_doRemove", "dAmnChat_onResize",
-    "dAmnChat_onData", "dAmnChat_onClose",
-    "dAmnChat_onDisconnect", "dAmnChat_onShutdown",
-    "dAmnChat_Send", "dAmnChat_SetElement", "dAmnChatMembers",
-    "dAmnChatMembers_SortMember",
-    "dAmnChatMembers_MatchMembers",
-    "dAmnChatMembers_ClearMembers",
-    "dAmnChatMembers_AddMember", "dAmnChatMembers_DelMember",
-    "dAmnChatMembers_MoveMember", "dAmnChatMembers_Refresh",
-    "dAmnChat_onKey", "dAmnChatInput",
-    "dAmnChatInput_toggleInput", "dAmnChatInput_onKey",
-    "dAmnChatInput_setFocus", "dAmnChatInput_appendText",
-    "dAmnChat_enterInfoCtr", "dAmnChat_leaveInfoCtr",
-    "dAmn_printAvatar", "dAmnChatMember",
-    "dAmnChatMember_updateInfo", "dAmnChatMember_updateHTML",
-    "dAmnChat_EmbededMedia_OnClick", "dAmnChanChat",
-    "dAmnChatbase_getTextNodesIn",
-    "dAmnChatbase_returnwordwraped",
-    "dAmnChatbase_updateAlerts", "dAmnChatbase_removeAlert",
-    "dAmnChatbase_onHide", "dAmn_addTimedDiv",
-    "dAmnChanMainChat", "dAmn_newRoomClick",
-    "dAmn_Plugin_timeout"
-  ];
 
   dAmn.event = {};
   dAmn.event.hook = function(method){
@@ -119,13 +50,20 @@ function DCDScript(){
           original[step] = dAmn.replaced[method];
         }
       });
+      if(method.indexOf("dAmnChanChat.prototype.") == 0){
+        dAmnChanMainChat.prototype[path[path.length-1]] = dAmn.replaced[method];
+        var channels = dAmn.chat.chatrooms;
+        for(var c in channels){
+          channels[c].channels.main[path[path.length-1]] = dAmn.replaced[method];
+        }
+      }
     }
   }
   dAmn.event.listeners = {};
   dAmn.event.emit = function(method){
     if(Array.isArray(dAmn.event.listeners[method])){
       var args = Array.prototype.slice.call(arguments, 1);
-      var listeners = dAmn.events.listeners[method];
+      var listeners = dAmn.event.listeners[method];
       for(var i=0; i<listeners.length; i++){
         listeners[i].apply(this, args);
       }
@@ -140,14 +78,40 @@ function DCDScript(){
   };
 
   dAmn.chat = {};
+  dAmn.chat.events = {};
+  var methods = {
+    onMsg: "dAmnChanChat.prototype.onMsg",
+    onAction: "dAmnChanChat.prototype.onAction",
+    onResize: "dAmnChanChat.prototype.onResize",
+    Clear: "dAmnChanChat.prototype.Clear",
+    onSmileyClick: "dAmnChanChat.prototype.onSmileyClick",
+    onSmiley: "dAmnChanChat.prototype.onSmiley",
+    takeFocus: "dAmnChanChat.prototype.takeFocus",
+    setTopic: "dAmnChanChat.prototype.setTopic",
+    makeText: "dAmnChanChat.prototype.makeText",
+    FormatMsg: "dAmnChanChat.prototype.FormatMsg"
+  };
+
+  for(var m in methods){
+    dAmn.chat.events[m] = function(handler){
+      return dAmn.event.listen(methods[m], handler);
+    }
+  }
+
   dAmn.chat.chatrooms = dAmnChats;
   dAmn.chat.tabs = dAmnChatTabs;
   dAmn.chat.stack = dAmnChatTabStack;
-  dAmn.chat.getActive = function(returnObject){
-    if(returnObject){
-      return dAmn.chat.chatrooms[dAmnChatTab_active];
+  dAmn.chat.getActive = function(returnChatroom){
+    if(returnChatroom){
+      return dAmn.chat.get(dAmnChatTab_active);
     }
     return dAmnChatTab_active;
+  }
+  dAmn.chat.get = function(ns){
+    if(typeof ns == "undefined"){
+      ns = dAmnChatTab_active;
+    }
+    return dAmn.chat.chatrooms[ns];
   }
   dAmn.chat.getTab = function(chatroom){
     if(typeof chatroom == "string"){
@@ -155,7 +119,7 @@ function DCDScript(){
     }
     return dAmn.chat.tabs[dAmn.chat.getActive()];
   }
-
+  
 }
 
 function execute_script(script, id){
