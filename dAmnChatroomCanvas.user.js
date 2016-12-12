e3d4e5dac98ebc657d9edfa986652e93f83964b7
Sam Mulqueen <sam.mulqueen.nz@gmail.com>
2016-12-13 03:35:38 +1300

    Updated version of userscript

--- dAmnChatroomCanvas.user.js
+++ dAmnChatroomCanvas.user.js
@@ -2,7 +2,7 @@
 // @name           dAmn Chatroom Canvas
 // @description    Draw alongside other Deviants right from within dAmn
 // @author         Sam Mulqueen <sammulqueen.nz@gmail.com>
-// @version        1.7.0
+// @version        1.7.1
 // @include        http://chat.deviantart.com/chat/*
 // ==/UserScript==
 
