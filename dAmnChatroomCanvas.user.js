5ab2ce596ab818716008e35b8a3405782600de33
Sam Mulqueen <sam.mulqueen.nz@gmail.com>
2017-01-08 14:50:51 +1300

    Updated dAmn_Helper_Script to most up to date version

--- dAmnChatroomCanvas.user.js
+++ dAmnChatroomCanvas.user.js
@@ -2,7 +2,7 @@
 // @name           dAmn Chatroom Canvas
 // @description    Draw alongside other Deviants right from within dAmn
 // @author         Sam Mulqueen <sammulqueen.nz@gmail.com>
-// @version        1.9.1
+// @version        1.9.2
 // @include        http://chat.deviantart.com/chat/*
 // ==/UserScript==
 
@@ -895,105 +895,122 @@ function dAmnHelper_Script(){
 
     dAmn.event = {};
     dAmn.event.hook = function(method){
-      var original = window;
-      var path = method.split(".");
-      path.forEach(function(step){
-        original = original[step];
-      });
-      if(typeof original == "function" && typeof dAmn.replaced[method] == "undefined"){
-        dAmn.original[method] = original;
-        dAmn.replaced[method] = function(){
-          var args = Array.prototype.slice.call(arguments);
-          var prevent = false;
-          var after_calls = [];
-          var event = {
-            args: args,
-            preventDefault: function(){
-              prevent = true;
-            },
-            returnValue: undefined,
-            after: function(fn){
-              if(typeof fn == "function"){
-                after_calls.push(fn);
-              }
-            }
-          };
-          dAmn.event.emit.apply(this, [method, event]);
-          var methodReturn;
-          if(!prevent){
-            if(typeof this == "function"){
-              methodReturn = dAmn.original[method].apply(this, args);
-            }else{
-              if(path[1] == "prototype"){
-                methodReturn = dAmn.original[method].apply(this, args);
-              }else{
-                methodReturn = new dAmn.original[method](args[0],args[1],args[2],args[3],args[4],args[5],args[6]);
-                for(var attr in methodReturn){
-                  this[attr] = methodReturn[attr];
+      try{
+        var original = window;
+        var path = method.split(".");
+        path.forEach(function(step){
+          original = original[step];
+        });
+        if(typeof original == "function" && typeof dAmn.replaced[method] == "undefined"){
+          dAmn.original[method] = original;
+          dAmn.replaced[method] = function(){
+            try{
+              var args = Array.prototype.slice.call(arguments);
+              var prevent = false;
+              var after_calls = [];
+              var event = {
+                args: args,
+                preventDefault: function(){
+                  prevent = true;
+                },
+                returnValue: undefined,
+                after: function(fn){
+                  if(typeof fn == "function"){
+                    after_calls.push(fn);
+                  }
+                }
+              };
+              dAmn.event.emit.apply(this, [method, event]);
+              var methodReturn;
+              if(!prevent){
+                if(typeof this == "function"){
+                  methodReturn = dAmn.original[method].apply(this, args);
+                }else{
+                  if(path[1] == "prototype"){
+                    methodReturn = dAmn.original[method].apply(this, args);
+                  }else{
+                    methodReturn = new dAmn.original[method](args[0],args[1],args[2],args[3],args[4],args[5],args[6]);
+                    for(var attr in methodReturn){
+                      this[attr] = methodReturn[attr];
+                    }
+                  }
+                }
+                if(typeof event.after == "function"){
+                  after_calls.forEach(function(fn){
+                    try{
+                      fn.call(this, methodReturn);
+                    }catch(ex){
+                      console.error("dAmn Helper Error (event.hook.after): "+ex);
+                    }
+                  });
                 }
               }
+              if(typeof this == "function" || path[1] == "prototype"){
+                return (typeof event.returnValue == "undefined") ? methodReturn : event.returnValue;
+              }
+            }catch(ex){
+              console.error("dAmn Helper Error (event.replacedMethod): "+ex);
             }
-            if(typeof event.after == "function"){
-              after_calls.forEach(function(fn){
-                fn.call(this, methodReturn);
-              });
+          };
+          if(["dAmnChat", "dAmnChanChat", "dAmnChanMainChat", "dAmnChatInput"].indexOf(method)>-1){
+            dAmn.replaced[method].prototype = dAmn.original[method].prototype;
+          }
+          original = window;
+          path.forEach(function(step, i){
+            if(i == path.length-1){
+              original[step] = dAmn.replaced[method];
+            }else{
+              original = original[step];
             }
+          });
+          if(method.indexOf("dAmnChanChat.prototype.") === 0){
+            dAmnChanMainChat.prototype[path[path.length-1]] = dAmn.replaced[method];
+            dAmn.chat.forEach(function(chatroom){
+              chatroom.channels.main[path[path.length-1]] = dAmn.replaced[method];
+            });
           }
-          if(typeof this == "function" || path[1] == "prototype"){
-            return (typeof event.returnValue == "undefined") ? methodReturn : event.returnValue;
+          if(method.indexOf("dAmnChatInput.prototype.") === 0){
+            dAmn.chat.forEach(function(chatroom){
+              chatroom.channels.main.input[path[path.length-1]] = dAmn.replaced[method];
+            });
           }
-        };
-        if(["dAmnChat", "dAmnChanChat", "dAmnChanMainChat", "dAmnChatInput"].indexOf(method)>-1){
-          dAmn.replaced[method].prototype = dAmn.original[method].prototype;
-        }
-        original = window;
-        path.forEach(function(step, i){
-          if(i == path.length-1){
-            original[step] = dAmn.replaced[method];
-          }else{
-            original = original[step];
+          if(method.indexOf("dAmnChat.prototype.") === 0){
+            dAmn.chat.forEach(function(chatroom){
+              chatroom[path[path.length-1]] = dAmn.replaced[method];
+            });
           }
-        });
-        if(method.indexOf("dAmnChanChat.prototype.") === 0){
-          dAmnChanMainChat.prototype[path[path.length-1]] = dAmn.replaced[method];
-          dAmn.chat.forEach(function(chatroom){
-            chatroom.channels.main[path[path.length-1]] = dAmn.replaced[method];
-          })
         }
-        if(method.indexOf("dAmnChatInput.prototype.") === 0){
-          dAmn.chat.forEach(function(chatroom){
-            chatroom.channels.main.input[path[path.length-1]] = dAmn.replaced[method];
-          })
-        }
-        if(method.indexOf("dAmnChat.prototype.") === 0){
-          dAmn.chat.forEach(function(chatroom){
-            chatroom[path[path.length-1]] = dAmn.replaced[method];
-          })
-        }
+      }catch(ex){
+        console.error("dAmn Helper Error (event.hook): "+ex);
       }
     };
     dAmn.event.listeners = {};
     dAmn.event.emit = function(method){
-      if(Array.isArray(dAmn.event.listeners[method])){
-        var args = Array.prototype.slice.call(arguments, 1);
-        var listeners = dAmn.event.listeners[method];
-        for(var i=0; i<listeners.length; i++){
-          listeners[i].apply(this, args);
+      try{
+        if(Array.isArray(dAmn.event.listeners[method])){
+          var args = Array.prototype.slice.call(arguments, 1);
+          var listeners = dAmn.event.listeners[method];
+          for(var i=0; i<listeners.length; i++){
+            listeners[i].apply(this, args);
+          }
         }
+      }catch(ex){
+        console.error("dAmn Helper Error (event.emit): "+ex);
       }
     };
     dAmn.event.listen = function(method, handler){
-      if(!Array.isArray(dAmn.event.listeners[method])){
-        dAmn.event.listeners[method] = [];
+      try{
+        if(!Array.isArray(dAmn.event.listeners[method])){
+          dAmn.event.listeners[method] = [];
+        }
+        dAmn.event.hook(method);
+        dAmn.event.listeners[method].push(handler);
+      }catch(ex){
+        console.error("dAmn Helper Error (event.listen): "+ex);
       }
-      dAmn.event.hook(method);
-      dAmn.event.listeners[method].push(handler);
     };
 
     dAmn.chat = {};
-    dAmn.chat.activate = function(ns){
-      dAmnChatTabs_activate(ns);
-    }
     dAmn.chat.events = {};
     var methods = {
       onMsg: "dAmnChanChat.prototype.onMsg",
@@ -1006,85 +1023,106 @@ function dAmnHelper_Script(){
       setTopic: "dAmnChanChat.prototype.setTopic",
       makeText: "dAmnChanChat.prototype.makeText",
       FormatMsg: "dAmnChanChat.prototype.FormatMsg",
-      Send: "dAmnChat.prototype.Send"
+      Send: "dAmnChat.prototype.Send",
+      onSelfEvent: "dAmnChanMainChat.prototype.onSelfEvent"
+    };
+
+    try{
+      for(var m in methods){
+        dAmn.chat.events[m] = (function(method){
+          return function(handler){
+            return dAmn.event.listen(method, handler);
+          };
+        })(methods[m]);
+      }
+    }catch(ex){
+      console.error("dAmn Helper Error (chat.events.listen): "+ex);
+    }
+
+    dAmn.resetHistory = function(el){
+      if (this.history_pos != -1  && this.history[this.history_pos] == el.value) { // posting from history.. move to the end
+        var before = this.history.slice(0,this.history_pos);
+        var after  = this.history.slice(this.history_pos+1);
+        this.history = before.concat(after).concat( this.history[this.history_pos] );
+      } else {
+        // add to history -- limit to 300
+        this.history = this.history.concat( el.value );
+        if( this.history.length > 300 )
+          this.history = this.history.slice(1);
+      }
+      this.history_pos = -1;
     };
 
     dAmn.command = function(name, argsRequired, onCommand){
-      if(typeof onCommand != "function"){
-        throw "dAmn.command: onCommand must be a function";
-      }
-      if(typeof name != "string"){
-        throw "dAmn.command: name must be a string"
-      }
-      name = name.toLowerCase();
-      argsRequired = argsRequired?1:0;
-
-      dAmn.event.listen("dAmnChatInput", function(event){
-        event.after(function(input){
-          input.cmds[name] = [argsRequired];
-        });
-      });
-
-      dAmn.chat.forEach(function(chatroom){
-        chatroom.channels.main.input.cmds[name] = [argsRequired];
-      });
-
-      function resetHistory(el){
-        if (this.history_pos != -1  && this.history[this.history_pos] == el.value) { // posting from history.. move to the end
-          var before = this.history.slice(0,this.history_pos);
-          var after  = this.history.slice(this.history_pos+1);
-          this.history = before.concat(after).concat( this.history[this.history_pos] );
-        } else {
-          // add to history -- limit to 300
-          this.history = this.history.concat( el.value );
-          if( this.history.length > 300 )
-            this.history = this.history.slice(1);
+      try{
+        if(typeof onCommand != "function"){
+          throw "dAmn.command: onCommand must be a function";
         }
-        this.history_pos = -1;
-      }
+        if(typeof name != "string"){
+          throw "dAmn.command: name must be a string";
+        }
+        name = name.toLowerCase();
+        argsRequired = argsRequired?1:0;
 
-      dAmn.event.listen("dAmnChatInput.prototype.onKey", function(event){
-        var e = event.args[0];
-        var kc = event.args[1];
-        var force = event.args[2];
-        var el = this.chatinput_el;
-        if(kc == 13 && ( force || !this.multiline || e.shiftKey || e.ctrlKey )){
-          if(el.value){
-            if(!(e.shiftKey || (!this.multiline && e.ctrlKey))){
-              var cmdre = el.value.match( /^\/([a-z]+)([\s\S]*)/m );
-              if(!cmdre){
-                return;
-              }
-              var cmd  = cmdre[1].toLowerCase();
-              var args = null;
-              if (cmdre[2]) {
-                var tmp = cmdre[2].match(/^\s([\s\S]*)/);
-                if( tmp && tmp.length )
-                  args = tmp[1];
-              }
-              if(cmd == name){
-                dAmnChatTabs_activate( this.cr.ns, true );
-                delete this.tablist;
-                resetHistory.call(this, el);
-                if(this.cmds[cmd][0]) {
-                  if (!args) {
-                    this.cr.channels.main.onErrorEvent( cmd, 'insufficient parameters' );
-                  }else{
-                    onCommand(args);
+        dAmn.event.listen("dAmnChatInput", function(event){
+          event.after(function(input){
+            input.cmds[name] = [argsRequired];
+          });
+        });
+
+        dAmn.chat.forEach(function(chatroom){
+          chatroom.channels.main.input.cmds[name] = [argsRequired];
+        });
+
+        dAmn.event.listen("dAmnChatInput.prototype.onKey", function(event){
+          try{
+            var e = event.args[0];
+            var kc = event.args[1];
+            var force = event.args[2];
+            var el = this.chatinput_el;
+            if(kc == 13 && ( force || !this.multiline || e.shiftKey || e.ctrlKey )){
+              if(el.value){
+                if(!(e.shiftKey || (!this.multiline && e.ctrlKey))){
+                  var cmdre = el.value.match( /^\/([a-z]+)([\s\S]*)/m );
+                  if(!cmdre){
+                    return;
                   }
-                }else{
-                  onCommand(args);
+                  var cmd  = cmdre[1].toLowerCase();
+                  var args = null;
+                  if (cmdre[2]) {
+                    var tmp = cmdre[2].match(/^\s([\s\S]*)/);
+                    if( tmp && tmp.length )
+                      args = tmp[1];
+                  }
+                  if(cmd == name){
+                    dAmnChatTabs_activate( this.cr.ns, true );
+                    delete this.tablist;
+                    dAmn.resetHistory.call(this, el);
+                    if(this.cmds[cmd][0]) {
+                      if (!args) {
+                        this.cr.channels.main.onErrorEvent( cmd, 'insufficient parameters' );
+                      }else{
+                        onCommand.call(this, args);
+                      }
+                    }else{
+                      onCommand.call(this, args);
+                    }
+                    el.value='';
+                    el.focus();
+                    event.preventDefault();
+                    event.returnValue = false;
+                  }
                 }
-                el.value='';
-                el.focus();
-                event.preventDefault();
-                event.returnValue = false;
               }
             }
+          }catch(ex){
+            console.error("dAmn Helper Error (command.onKey): "+ex);
           }
-        }
-      });
-    }
+        });
+      }catch(ex){
+        console.error("dAmn Helper Error (command): "+ex);
+      }
+    };
 
     dAmn.send = {};
     dAmn.send.msg = function(ns, text){
@@ -1101,15 +1139,52 @@ function dAmnHelper_Script(){
     };
     dAmn.send.join = function(ns){
       dAmn_Join(ns);
+    };
+    dAmn.send.part = function(ns){
+      dAmn_Part(ns?ns:dAmn.chat.getActive());
     }
-
-    for(var m in methods){
-      dAmn.chat.events[m] = (function(method){
-        return function(handler){
-          return dAmn.event.listen(method, handler);
-        };
-      })(methods[m]);
-    }
+    dAmn.send.kick = function(ns, user, reason){
+      dAmn_Kick( ns?ns:dAmn.chat.getActive(), user, dAmnEscape(reason) );
+    };
+    dAmn.send.ban = function(ns, user){
+      var chatroom = dAmn.chat.get(ns);
+      if(chatroom && chatroom.Send){
+        chatroom.Send("ban", user, "");
+      }
+    };
+    dAmn.send.unban = function(ns, user){
+      var chatroom = dAmn.chat.get(ns);
+      if(chatroom && chatroom.Send){
+        chatroom.Send("unban", user, "");
+      }
+    };
+    dAmn.send.promote = function(ns, user, privclass){
+      var chatroom = dAmn.chat.get(ns);
+      if(chatroom && chatroom.Send){
+        chatroom.Send("promote", user, privclass);
+      }
+    };
+    dAmn.send.demote = function(ns, user, privclass){
+      var chatroom = dAmn.chat.get(ns);
+      if(chatroom && chatroom.Send){
+        chatroom.Send("demote", user, privclass);
+      }
+    };
+    dAmn.send.whois = function(user){
+      dAmn_Get( 'login:'+user, 'info' );
+    };
+    dAmn.send.clear = function(ns){
+      var chatroom = dAmn.chat.get(ns);
+      if(chatroom && chatroom.channels.main.Clear){
+        chatroom.channels.main.Clear();
+      }
+    };
+    dAmn.send.title = function(ns, text){
+      dAmn_Set( ns?ns:dAmn.chat.getActive(), "title", dAmnEscape(text) );
+    };
+    dAmn.send.topic = function(ns, text){
+      dAmn_Set( ns?ns:dAmn.chat.getActive(), "topic", dAmnEscape(text) );
+    };
 
     dAmn.chat.chatrooms = dAmnChats;
     dAmn.chat.tabs = dAmnChatTabs;
@@ -1120,11 +1195,25 @@ function dAmnHelper_Script(){
       }
       return dAmnChatTab_active;
     };
+    dAmn.chat.activate = function(ns){
+      dAmnChatTabs_activate( ns, true );
+    };
     dAmn.chat.get = function(ns){
-      if(typeof ns != "string"){
-        ns = dAmnChatTab_active;
+      try{
+        if(typeof ns != "string"){
+          ns = dAmnChatTab_active;
+        }else{
+          for(var room in dAmn.chat.chatrooms){
+            if(room.toLowerCase() == ns.toLowerCase()){
+              ns = room;
+              break;
+            }
+          }
+        }
+        return dAmn.chat.chatrooms[ns];
+      }catch(ex){
+        console.error("dAmn Helper Error (chat.get): "+ex);
       }
-      return dAmn.chat.chatrooms[ns];
     };
     dAmn.chat.getTab = function(chatroom){
       if(typeof chatroom == "string"){
@@ -1150,7 +1239,7 @@ function dAmnHelper_Script(){
       var chatroom = dAmn.chat.get();
       spanClass = spanClass?spanClass:"";
       dAmn_addTimedDiv( chatroom.channels.main.error_el, "damn-error "+spanClass, str, timeout );
-    }
+    };
   }
 
   DWait.ready(['jms/pages/chat07/chatpage.js', 'jms/pages/chat07/dAmn.js', 'jms/pages/chat07/dAmnChat.js'], function() {
